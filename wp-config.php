c7512b4c9bb30d33ffa5f9bc886f752f08aa1bcd
Tristan Hipolito <tristan.hipolito@pantheon.io>
2022-04-04 02:00:59 +0800

    Add Primary domain via wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -91,7 +91,27 @@ if (isset($_ENV['PANTHEON_ENVIRONMENT']) && php_sapi_name() != 'cli') {
   if ($_SERVER['HTTP_HOST'] != $primary_domain) {
     $requires_redirect = true;
   }
+
+  // If you're not using HSTS in the pantheon.yml file, uncomment this next block.
+  // if (!isset($_SERVER['HTTP_USER_AGENT_HTTPS'])
+  //     || $_SERVER['HTTP_USER_AGENT_HTTPS'] != 'ON') {
+  //   $requires_redirect = true;
+  // }
+
+  if ($requires_redirect === true) {
+
+    // Name transaction "redirect" in New Relic for improved reporting (optional).
+    if (extension_loaded('newrelic')) {
+      newrelic_name_transaction("redirect");
+    }
+
+    header('HTTP/1.0 301 Moved Permanently');
+    header('Location: https://'. $primary_domain . $_SERVER['REQUEST_URI']);
+    exit();
+  }
 }
+
+
 
 
 
